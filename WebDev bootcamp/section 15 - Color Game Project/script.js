b62859d4ec3083ebd796cdcfd592a4454f778643
MaciejLitwiniec <litwiniec.maciej@gmail.com>
2017-10-12 20:38:20 +0200

    added reset button with underlying logic

--- WebDev bootcamp/section 15 - Color Game Project/script.js
+++ WebDev bootcamp/section 15 - Color Game Project/script.js
@@ -4,7 +4,23 @@ var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
+var resetButtun = document.querySelector("#reset");
 var h1 = document.querySelector("h1");
+
+resetButtun.addEventListener("click", function(){
+  //generate new colors
+  colors = generateRandomColors(6);
+  //pick new random collor from array
+  pickedColor = pickColor();
+  //change colorDisplay to maatch picked color
+  colorDisplay.textContent = pickedColor;
+  //change colors of squares on page
+  for(var i = 0; i < squares.length; i++){
+      squares[i].style.backgroundColor = colors[i];
+  }
+  h1.style.backgroundColor = "#232323";
+
+});
 
 colorDisplay.textContent = pickedColor;
 
@@ -12,18 +28,19 @@ colorDisplay.textContent = pickedColor;
 //looping through squares (divs) and assigning colors from array
 for(var i = 0; i < squares.length; i++){
   //add initial colors to squares
-  squares[i].style.background = colors[i];
+  squares[i].style.backgroundColor = colors[i];
 
   //add click listeners to squares
   squares[i].addEventListener("click", function(){
 
     //grab color of clicked color
-    var clickedColor = this.style.background;
+    var clickedColor = this.style.backgroundColor;
     //compare color to pickedColor
     if (clickedColor === pickedColor) {
       messageDisplay.textContent = "Correct!";
+      resetButtun.textContent = "Play again!";
       changeColors(clickedColor);
-      h1.style.background = clickedColor;
+      h1.style.backgroundColor = clickedColor;
     } else {
       this.style.backgroundColor = "#232323";
       messageDisplay.textContent = "Try Again!";
@@ -35,7 +52,7 @@ function changeColors(color){
   //loop through all squares
   for (var i = 0; i < squares.length; i++){
     //change each color to match given color
-    squares[i].style.background = color;
+    squares[i].style.backgroundColor = color;
   }
 }
 
